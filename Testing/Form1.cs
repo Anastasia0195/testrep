e9e5aa438235740cd3b24237f55f0202ee27e723
Anastasia0195 <Anastasia0195@yandex.ru>
2016-10-18 16:53:49 +0300

    test1

---
+++ Testing/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Testing
+{
+    public partial class Form1 : Form
+    {
+        TestingProvider tp;
+        public Form1()
+        {
+            InitializeComponent();
+         tp = new TestingProvider("filetest.txt");
+            QuestionLabel.Text = tp.CurrentQuestion.QuestionText;
+            firstRadio.Text = tp.CurrentQuestion.Answers[0].Answer;
+            secondRadio.Text = tp.CurrentQuestion.Answers[1].Answer;
+            thirdRadio.Text = tp.CurrentQuestion.Answers[2].Answer;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!tp.IsEnded)
+            {
+                int rightCheck;
+                if (firstRadio.Checked)
+                    rightCheck = 1;
+                else if (secondRadio.Checked)
+                    rightCheck = 2;
+                else if (thirdRadio.Checked)
+                    rightCheck = 3;
+                else rightCheck = 1;
+
+                tp.NextQuestion(rightCheck);
+                if (!tp.IsEnded)
+                {
+                    QuestionLabel.Text = tp.CurrentQuestion.QuestionText;
+                    firstRadio.Text = tp.CurrentQuestion.Answers[0].Answer;
+                    secondRadio.Text = tp.CurrentQuestion.Answers[1].Answer;
+                    thirdRadio.Text = tp.CurrentQuestion.Answers[2].Answer;
+                }
+                else { MessageBox.Show(tp.FinalMessage); }
+            }
+        }
+    }
+}
--- Testing/Properties/AssemblyInfo.cs
+++ Testing/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Управление общими сведениями о сборке осуществляется с помощью 
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("test")]
+[assembly: AssemblyTitle("Testing")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("test")]
+[assembly: AssemblyProduct("Testing")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("4cc57b9e-a5a9-4b7f-9c39-69b97675806f")]
+[assembly: Guid("279d135e-a55e-40c0-a27d-4004c2dc3a31")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
